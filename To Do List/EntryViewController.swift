6195ce58f0f521bbd24b78a815240526b4f02fc8
Nguyen Xuan An <lanehacker7294@yahoo.com>
2020-05-07 19:15:35 +0700

    2nd

--- To Do List/EntryViewController.swift
+++ To Do List/EntryViewController.swift
@@ -12,6 +12,8 @@ class EntryViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet var field: UITextField!
     
+    var update: (() -> Void)?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         field.delegate = self
@@ -26,7 +28,20 @@ class EntryViewController: UIViewController, UITextFieldDelegate {
     
     @objc func saveTask(){
         
-        guard let text
+        guard let text = field.text, !text.isEmpty else {
+            return
+        }
+        
+        guard let count = UserDefaults().value(forKey: "count") as? Int else {
+            return
+        }
+        
+        let newCount = count + 1
+        
+        UserDefaults().set(newCount, forKey: "count")
+        UserDefaults().set(text, forKey: "task_\(newCount)")
+        
+        update?()
+        navigationController?.popViewController(animated: true)
     }
-    
 }
--- To Do List/ViewController.swift
+++ To Do List/ViewController.swift
@@ -20,11 +20,41 @@ class ViewController: UIViewController {
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        // Setup
+        
+        if !UserDefaults().bool(forKey: "setup"){
+            UserDefaults().set(true, forKey: "setup")
+            UserDefaults().set(0, forKey: "count")
+        }
+        
+        // Get all current saved tasks
+        updateTasks()
+    }
+    
+    func updateTasks(){
+        tasks.removeAll()
+        guard let count = UserDefaults().value(forKey: "count") as? Int else{
+            return
+        }
+        
+        for x in 0..<count{
+            if let task = UserDefaults().value(forKey: "task_\(x+1)") as? String {
+              tasks.append(task)
+            }
+        }
+        
+        tableView.reloadData()
     }
 
     @IBAction func didTapAdd(){
         let vc = storyboard?.instantiateViewController(withIdentifier: "entry") as! EntryViewController
         vc.title = "New Task"
+        vc.update = {
+            DispatchQueue.main.async {
+                self.updateTasks()
+            }
+        }
         navigationController?.pushViewController(vc, animated: true)
     }
 
@@ -33,6 +63,10 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDelegate{
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        
+        let vc = storyboard?.instantiateViewController(withIdentifier: "task") as! TaskViewController
+        vc.title = "New Task"
+        navigationController?.pushViewController(vc, animated: true)
     }
 }
 
